6fe9fafa8fc91def264a243f244e1d0fc48f60e1
Aryan Singh <reachforaryan@users.noreply.github.com>
2026-07-20 14:22:40 +0530

    Initial Commit

---
+++ UtilityToggle/UtilityToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtilityToggleApp.swift
+//  UtilityToggle
+//
+//  Created by Aryan Singh on 20/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UtilityToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
